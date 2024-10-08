7199f4cfe8e2d4980e096b45cf1930466168552e
micky-code <solaisan22@.com>
2024-10-08 14:27:06 +0700

    test

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h2>{{ msg }}</h2>
+      <button @click="sendDataToParent">Send Data to Parent</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['msg'],
+    methods: {
+      sendDataToParent() {
+        this.$emit('childEvent', 'Hello Parent, from Child!');
+      }
+    }
+  };
+  </script>
+  
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <p>Count: {{ count }}</p>
+      <button @click="increment">Increment</button>
+    </div>
+  </template>
+  
+  <script>
+  import { useMainStore } from '../store/index';
+  import { computed } from 'vue'; // Add computed to handle reactivity
+  
+  export default {
+    setup() {
+      const store = useMainStore();
+  
+      // Use computed to make the count reactive
+      const count = computed(() => store.count);
+  
+      return {
+        count,
+        increment: store.increment
+      };
+    }
+  };
+  </script>
+  
